a1ed065ca3d661ac42f88a7a4f19877dbde9cece
Raghav893 <raghavarora2309@gmail.com>
2025-03-21 15:09:57 +0530

    using another file for data

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,30 +1,3 @@
-const products =[{
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        star: 4.5,
-        count:87
-    },
-    price_cents:1090
-    },
-    {image:'images/products/intermediate-composite-basketball.jpg',
-    name:'Intermediate Size Basketball',
-    rating:{
-        star:4,
-        count:127
-        },
-        price_cents:2095,
-    },
-    {image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        star:4.5,
-        count:56
-        },
-        price_cents:799,
-    }];
-
-
 let  productshtml='';
     products.forEach(product=>{
         productshtml+=`
@@ -40,14 +13,14 @@ let  productshtml='';
 
             <div class="product-rating-container">
                 <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.star*10}.png">
+                src="images/ratings/rating-${product.rating.stars*10}.png">
                 <div class="product-rating-count link-primary">
                 ${product.rating.count}
                 </div>
             </div>
 
             <div class="product-price">
-            $ ${product.price_cents/10}
+            $ ${(product.priceCents/100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
